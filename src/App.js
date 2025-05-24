5aa6b7ed237d58b6f2e55441d63a1bd9b95cd8de
NGUYEN Thien Huy <ngthienhuy08@gmail.com>
2025-05-24 17:03:59 +0200

    Add gitignore and switch to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js - Final version with all components
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 // Import components
